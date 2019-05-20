1fa6d903cd8e189e672f70db640cee3ea2e9cbd2
Nastia Klitsuk <nastiak@codevalue.net>
2019-05-20 08:49:34 +0300

    package.json was added

--- exercise1/server.js
+++ exercise1/server.js
@@ -7,6 +7,9 @@ function init() {
       utils.sleep(2000);
       res.writeHead(200);
       res.end('ok\n');
+      console.log(
+        `Server response processing completed on ${utils.getCurrentFormattedDate()}.`
+      );
     })
     .listen(8001);
   console.log('Server is up.');
